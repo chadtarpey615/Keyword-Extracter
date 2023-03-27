2c9044a532970bd198568a8ee1ea7dc60304f630
Chad Tarpey <chadtarpey@gmail.com>
2023-03-26 20:37:03 -0500

    created textinput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Box } from '@chakra-ui/react'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import TextInput from './components/TextInput'
+
 
 const App = () => {
+
+const [keywords, setKeywords] = useState('')
+const [isOpened, setIsOpened] = useState(false)
+const [loading, setLoading] = useState(false)
+
+  const extractKeywords = (text) => {
+    setLoading(true)
+    setIsOpened(true)
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify({
+      model: 'text-davinci-003',
+      prompt: 'Extract Keywords from this text. Make the first letter of each word uppercase and seperate with commas\n\n' + text + '',
+      temperature: 0.5,
+      max_tokens: 60,
+      frequency_penalty: 0.8,
+    })
+  }
+}
+
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='container.xl' centerContent>
       <Header />
+      <TextInput extractKeywords={extractKeywords}/>
+      <Footer />
       </Container>
     </Box>
   )
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,33 @@
+import {useState} from 'react'
+import {Button, Textarea, useToast} from '@chakra-ui/react'
+
+const TextInput = ({extractKeywords}) => {
+    const [text, setText] = useState('')
+
+    const toast = useToast()
+
+    const submitText = () => {
+      if (text === '') {
+        toast({
+            title: 'No Text Entered',
+            description: 'Please enter some text to extract keywords from.',
+            status: 'error',
+            duration: 5000,
+            isClosable: false,
+        })
+        } else {
+            extractKeywords(text)
+      }
+    }
+
+  return (
+    <>
+    <Textarea bg='blue.400' color='white' padding={4} height={200} marginTop={6} onChange={ (e) => setText(e.target.value)}/>
+      <Button bg='blue.500' color="white" marginTop={4} width='100%' _hover={{ bg: 'blue.700'}} onClick={submitText}>
+        Extract Keywords
+      </Button>
+    </>
+  )
+}
+
+export default TextInput
